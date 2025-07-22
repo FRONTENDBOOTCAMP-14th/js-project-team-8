d130d2255c5590cb72dece10e45a5c6d97014d94
rhocci <4tres1@gmail.com>
2025-07-22 16:15:48 +0900

    feat: Input 컴포넌트 구현
    - id, type, label, variant, placeholder 커스텀 인자 포함
    - 텍스트 인풋/검색 인풋 대응 가능

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,5 +1,44 @@
 import './Input.css';
 
-export function Input({ value, placeholder = '입력하기', type = 'text' }) {
-  // 여기 안에 컴포넌트 작성
+export function Input({
+  id = '',
+  type = 'text',
+  label = '',
+  variant = 'default',
+  placeholder = '',
+}) {
+  const inputWrapper = document.createElement('div');
+  const inputLabel = document.createElement('label');
+  const input = document.createElement('div');
+  const icon = document.createElement('img');
+  const inputField = document.createElement('input');
+
+  inputWrapper.className = 'input-wrapper';
+
+  if (label) {
+    inputLabel.className = 'input-label';
+    inputLabel.htmlFor = id;
+    inputLabel.textContent = label;
+
+    inputWrapper.append(inputLabel);
+  }
+
+  input.className = `input input-${variant}`;
+
+  inputField.className = 'input-field';
+  inputField.id = id;
+  inputField.type = type;
+  inputField.placeholder = placeholder;
+
+  if (variant === 'search') {
+    icon.src = '/src/assets/icon/search.svg';
+    icon.alt = '검색 아이콘';
+
+    input.append(icon);
+  }
+
+  input.append(inputField);
+  inputWrapper.append(input);
+
+  return inputWrapper;
 }
