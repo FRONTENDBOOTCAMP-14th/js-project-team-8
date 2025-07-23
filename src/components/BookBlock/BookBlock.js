ac9105b152d6cab8728077773f1bdcf42c373d83
rhocci <4tres1@gmail.com>
2025-07-23 15:13:56 +0900

    feat: Bookblcok 컴포넌트 구현

--- src/components/BookBlock/BookBlock.js
+++ src/components/BookBlock/BookBlock.js
@@ -1,8 +1,28 @@
 import './BookBlock.css';
 
-export function BookBlock() {
+export function BookBlock({ title = '책 제목', desc = '한줄 서평', index = 0, onClick = null }) {
   const bookBlock = document.createElement('div');
+  const bookTitle = document.createElement('h3');
+  const bookDesc = document.createElement('p');
 
+  const direction = ['bookblock-right', 'bookblock-left'];
+  const color = ['bookblock-blue', 'bookblock-yellow', 'bookblock-green', 'bookblock-pink'];
+
+  bookBlock.className = 'bookblock';
+  bookTitle.className = 'bookblock-title';
+  bookDesc.className = 'bookblock-desc';
+
+  bookBlock.classList.add(direction[index % direction.length]);
+  bookBlock.classList.add(color[index % color.length]);
+
+  bookTitle.textContent = title;
+  bookDesc.textContent = desc;
+
+  bookBlock.append(bookTitle, bookDesc);
+
+  if (onClick) {
+    bookBlock.addEventListener('click', onClick);
+  }
 
   return bookBlock;
-};
+}
