a2e4a4c20ef6024e4362abe77b832a1fb5ffbc7c
rhocci <4tres1@gmail.com>
2025-07-29 18:12:36 +0900

    feat: 대시보드 로그인 토큰 여부에 따른 페이지 리디렉션

--- src/api/dashboardData.js
+++ src/api/dashboardData.js
@@ -1,6 +1,20 @@
 /** 대시보드 API 요청 */
 export async function fetchDashboardData() {
-  const token = localStorage.getItem('token');
+  // TODO: 로그인 구현되면 아래 구문으로 교체
+  // const token = localStorage.getItem('token');
+  const token = '임시 토큰';
+
+  if (!token) {
+    const answer = confirm('로그인이 필요합니다. 로그인 페이지로 이동할까요?');
+
+    const redirectUrl = answer
+      ? '/src/pages/LoginAndSignUp/LoginAndSignUp.html'
+      : '/src/pages/Community/Community.html';
+
+    window.location.replace(redirectUrl);
+    return;
+  }
+
   const res = await fetch('https://server.bookmark.soop.run/dashboard', {
     method: 'GET',
     headers: {
