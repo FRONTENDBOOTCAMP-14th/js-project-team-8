c1e0331f90dbe2f8597b20441b0fa7b2866da1ab
chacokyo <3jisoo5@naver.com>
2025-07-30 17:56:12 +0900

    feat: Community 페이지내 API 연동, 필터링 함수 추가

--- src/pages/Community/Community.js
+++ src/pages/Community/Community.js
@@ -1,4 +1,6 @@
 import './Community.css';
+
+import { fetchCommunityData } from '../../api/communityData';
 import { Sidebar } from '../../components/Sidebar/Sidebar';
 import { Title } from '../../components/Title/Title';
 import { Carousel } from '../../components/Carousel/Carousel';
@@ -6,15 +8,56 @@ import { Button } from '../../components/Button/Button';
 import { BookHover } from '../../components/BookHover/BookHover';
 
 export function initCommunity() {
+
+  // 서버에서 받아온 책 데이터 저장
+  let bookDataList = []
+  const bookWrapper = document.createElement('div');
+  bookWrapper.className = 'community-book-wrapper';
+
+  /** 책 데이터를 받아와서 화면에 렌더링하는 비동기 함수 */
+  async function loadBookData(sortType = 'latest') {
+    try {
+      const data = await fetchCommunityData();
+      bookDataList = data.publicReviews || [];
+      renderBooks(sortType);
+    } catch (error) {
+      console.error(error.message);
+      return null
+    }
+  }
+
+  /** 정렬 기준(최신순, 제목순)에 따른 필터링 함수 */
+  function renderBooks(sortType = 'latest') {
+    if (!bookDataList.length) return;
+
+    let sorted = [...bookDataList];
+
+    if (sortType === 'title') {
+      sorted.sort((a, b) => a.title.localeCompare(b.title, 'ko'));
+    } else {
+      sorted.sort((a, b) => new Date(b.date) - new Date(a.date));
+    }
+
+    bookWrapper.innerHTML = '';
+
+    sorted.forEach((book) => {
+      bookWrapper.append(
+        BookHover({
+          title: book.title,
+          reviewTitle: book.oneLineDescription,
+          id: book.nickname,
+          imageUrl: book.imageUrl,
+        })
+      );
+    });
+  }
+
   const community = document.getElementById('community');
   const header = document.querySelector('.community-header');
   const wrapper = document.querySelector('.community-wrapper');
 
   const btnWrapper = document.createElement('div');
   btnWrapper.className = 'community-btn-wrapper';
-
-  const bookWrapper = document.createElement('div');
-  bookWrapper.className = 'community-book-wrapper';
 
   const promotionTitle = ['너무 늦은 시간', '혼모노', '자몽살구클럽', '모순', '새로운 질서'];
   const promotionTitleBlock = Title({ text: `# ${promotionTitle[0]}`, color: 'promotion' });
@@ -44,10 +87,16 @@ export function initCommunity() {
       btn.classList.toggle('btn-dark', btn === selectedBtn);
       btn.classList.toggle('btn-gray', btn !== selectedBtn);
     });
+
+    const sortType = selectedBtn.textContent === '제목순' ? 'title' : 'latest';
+    renderBooks(sortType);
   });
 
   wrapper.append(Carousel(), btnWrapper, bookWrapper);
   community.prepend(Sidebar({ selectedIndex: 2 }));
+
+  // 데이터 로딩은 따로 분리된 함수에서 처리
+  loadBookData(); // 최신순으로 초기 렌더링
 }
 
 initCommunity();
