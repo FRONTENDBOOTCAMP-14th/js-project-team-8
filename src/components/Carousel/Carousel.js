b20b135781c61c07fb73fc3ee8b243f618e4af9f
chacokyo <3jisoo5@naver.com>
2025-07-30 10:34:16 +0900

    refactor: Carsousel의 슬라이드 데이터 리펙토링 - 슬라이드 데이터는 유지 보수를 위해 따로 파일을 분리함

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,5 +1,6 @@
 import './Carousel.css';
 import { Button } from '../Button/Button.js';
+import { slideData } from '../../data/carouselData.js';
 
 export function Carousel() {
   const carousel = document.createElement('section');
@@ -10,33 +11,6 @@ export function Carousel() {
 
   const slideWrapper = document.createElement('ul');
   slideWrapper.className = 'carousel-slide-wrapper';
-
-  const slideData = [
-    {
-      title1: '당신의 길에 작은',
-      title2: '힌트를 줄 책갈피',
-      subtitle: '흔들리는 하루 속, <br class="line-break">책은 작은 평온을 남겼다.',
-      src: new URL('../../assets/carousel/pc-banner01.png', import.meta.url).href,
-      alt: '당신의 길에 작은 힌트를 줄 책갈피',
-      href: '#',
-    },
-    {
-      title1: '흔들리는 청춘에 건네는',
-      title2: '따뜻한 위로의 책갈피',
-      subtitle: '불안한 일상속에서 <br class="line-break">책속의 한줄이 길이 되었다.',
-      src: new URL('../../assets/carousel/pc-banner02.png', import.meta.url).href,
-      alt: '흔들리는 청춘에 건네는 따뜻한 위로의 책갈피',
-      href: '#',
-    },
-    {
-      title1: '꿈을 향해 나아가는',
-      title2: '용기의 한마디',
-      subtitle: '책속의 주인공이 <br class="line-break">나를 다시 일으켜 세웠다.',
-      src: new URL('../../assets/carousel/pc-banner03.png', import.meta.url).href,
-      alt: '꿈을 향해 나아가는 용가의 책갈피',
-      href: '#',
-    },
-  ];
 
   const slides = slideData.map(({ title1, title2, subtitle, src, alt, href }) => {
     const slide = document.createElement('li');
