5a3fdedaaa1c5b4c72e3b061b19cff6941e7f5db
rhocci <4tres1@gmail.com>
2025-08-03 13:24:47 +0900

    feat: BookItem 키보드 선택 분기 추가

--- src/components/BookItem/BookItem.js
+++ src/components/BookItem/BookItem.js
@@ -10,9 +10,16 @@ export function BookItem({ onClick = null, imageUrl = null, title = '' }) {
   bookTitle.textContent = title;
 
   container.append(BookCover({ imageUrl: imageUrl }), bookTitle);
+  container.setAttribute('tabindex', '0');
 
   if (onClick && typeof onClick === 'function') {
     container.addEventListener('click', onClick);
+    container.addEventListener('keydown', (e) => {
+      if (e.key === 'Enter' || e.key === ' ') {
+        e.preventDefault();
+        onClick();
+      }
+    });
   }
 
   return container;
